4361b93124b692af35a6962a827db83b99ac8438
guillaume <guillaume.rahbari@gmail.com>
2018-05-13 08:57:56 +0200

    Update recipe controller

--- src/main/java/com/personal/recipe/controller/RecipeController.java
+++ src/main/java/com/personal/recipe/controller/RecipeController.java
@@ -1,14 +1,22 @@
 package com.personal.recipe.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/recipes")
 public class RecipeController {
 
-    @RequestMapping("/recipes")
+    @RequestMapping(method = RequestMethod.GET)
     String getRecipes() {
         return "Hello recipes";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    String getRecipe(@PathVariable String id) {
+        return id;
     }
 
 }
